7b5ccda32c5941a870073a2fdf29b100e386607f
wangjiayu <236524245@qq.com>
2017-12-05 16:20:14 +0800

    first-test

---
+++ test/js/app.js
@@ -0,0 +1,15 @@
+
+/**
+ * Created by Administrator on 2017/12/4.
+ */
+$(function () {
+    if(browserCompatibility == true){
+        indexInit();
+    }else{
+        $("body").css({display:"none"});
+    }
+});
+
+function indexInit() {
+
+}
